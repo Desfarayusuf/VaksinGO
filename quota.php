e9347a0690ca65ea1aae290fb02b757575930aeb
Risang Baskoro <risangbaskoro@gmail.com>
2021-12-09 10:26:47 +0700

    automated showing vaccine quota based on database

--- quota.php
+++ quota.php
@@ -20,68 +20,36 @@
             </h1>
         </div>
 
-        <div class="my-5">
-            <div class="d-flex flex-row">
-                <div class="w-50 my-auto">
-                    <h1 class="display-5 orange">Rumah Sakit Nasional Diponegoro</h1>
-                    <p>Alamat : Jl. Professor Haji Soedarto S.H, Tembalang, Kec. Tembalang, Kota Semarang, Jawa Tengah 50275</a>
-                    <p>Quota : 178 </p>
-                </div>
-                <div class="mx-auto">
-                    <img src="img/RSND.jpg" alt="">
-                </div>
-            </div>
-        </div>
-        <div class="my-5">
-            <div class="d-flex flex-row">
-                <div class="w-50 my-auto">
-                    <h1 class="display-5 orange">Rumah Sakit Umum Pusat Dr. Kariadi Semarang</h1>
-                    <p>Alamat : Jl. DR. Sutomo No.16, Randusari, Kec. Semarang Sel., Kota Semarang, Jawa Tengah 50244</a>
-                    <p>Quota : 123 </p>
-                </div>
-                <div class="mx-auto">
-                    <img src="img/RSK.jpg" alt="">
-                </div>
-            </div>
-        </div>
-        <div class="my-5">
-            <div class="d-flex flex-row">
-                <div class="w-50 my-auto">
-                    <h1 class="display-5 orange">Rumah Sakit Umum William Booth</h1>
-                    <p>Alamat : Jl. Letnan Jenderal S. Parman No.5, Petompon, Kec. Gajahmungkur, Kota Semarang, Jawa Tengah 50237</a>
-                    <p>Quota : 123 </p>
-                </div>
-                <div class="mx-auto">
-                    <img src="img/RSUWB.jpg" alt="">
-                </div>
-            </div>
-        </div>
-        <div class="my-5">
-            <div class="d-flex flex-row">
-                <div class="w-50 my-auto">
-                    <h1 class="display-5 orange">Rumah Sakit Permata Medika</h1>
-                    <p>Alamat : Jl. Raya Mr. Moch Ichsan No.93-97, Ngaliyan, Kec. Ngaliyan, Kota Semarang, Jawa Tengah 50181</a>
-                    <p>Quota : 123 </p>
-                </div>
-                <div class="mx-auto">
-                    <img src="img/RSMP.jpg" alt="">
-                </div>
-            </div>
-        </div>
-        <div class="my-5">
-            <div class="d-flex flex-row">
-                <div class="w-50 my-auto">
-                    <h1 class="display-5 orange">Rumah Sakit RS Telogorejo</h1>
-                    <p>Alamat : Jl. Kh Ahmad Dahlan, Pekunden, Kec. Semarang Tengah, Kota Semarang, Jawa Tengah 50134</a>
-                    <p>Quota : 123 </p>
-                </div>
-                <div class="mx-auto">
-                    <img src="img/RST.jpg" alt="">
-                </div>
-            </div>
-        </div>
-        <br>
-        <br>
+        <?php
+        include('config.php');
+        $query = $db->query('SELECT * FROM hospital');
+        if (!$query) {
+            die($db->errorInfo());
+        }
+        else {
+            while ($row = $query->fetchObject()){
+                $hospital_name = $row->name;
+                $hospital_address = $row->address;
+                $hospital_quota = $row->vaccine_quota;
+                $hospital_image = $row->image;
+
+
+                echo '<div class="my-5">';
+                    echo '<div class="d-flex flex-row">';
+                        echo '<div class="w-50 my-auto">';
+                            echo '<h1 class="display-5 orange">'.$hospital_name.'</h1>';
+                            echo '<p>'.$hospital_address.'</p>';
+                            echo '<p>Quota: '.$hospital_quota.' </p>';
+                        echo '</div>';
+                        echo '<div class="mx-auto">';
+                            echo '<img src="data:image/jpeg;base64,'.base64_encode($hospital_image).' alt="'.$hospital_name.'">';
+                        echo '</div>';
+                    echo '</div>';
+                echo '</div>';
+            }
+        }
+        ?>
+
         <div class="my-5">
             <div class="d-flex flex-row">
                 <div class="w-50 my-auto">
